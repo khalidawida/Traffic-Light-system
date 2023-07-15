35332586de69c517871cd67bfb1d95f51f02e362
khalidawida <139075710+khalidawida@users.noreply.github.com>
2023-07-15 23:14:05 +0100

    Add files via upload

---
+++ Bonus_Task.ino
@@ -0,0 +1,156 @@
+int ledPin_zero = 2;
+
+int ledPin_one = 3;
+
+int ledPin_two = 4;
+int ledPin_three = 5;
+int ledPin_four = 6;
+
+int TIME = 2000;
+
+
+void setup() {
+
+  pinMode(ledPin_zero, OUTPUT); 
+
+  pinMode(ledPin_one, OUTPUT); 
+
+  pinMode(ledPin_two, OUTPUT);
+  pinMode(ledPin_three, OUTPUT);
+  pinMode(ledPin_four, OUTPUT);
+
+}
+
+void loop() {
+
+  digitalWrite(ledPin_zero, LOW);
+    digitalWrite(ledPin_one, LOW);
+  digitalWrite(ledPin_two, LOW);
+  digitalWrite(ledPin_three, LOW);
+  digitalWrite(ledPin_four, LOW);
+ delay(TIME);
+    digitalWrite(ledPin_zero, HIGH);
+   delay(TIME);
+    digitalWrite(ledPin_zero, LOW);
+      digitalWrite(ledPin_one, HIGH);
+     delay(TIME);
+      digitalWrite(ledPin_zero, HIGH);
+      digitalWrite(ledPin_one, HIGH);
+     delay(TIME);
+   digitalWrite(ledPin_zero, LOW);
+      digitalWrite(ledPin_one, LOW);
+        digitalWrite(ledPin_two, HIGH);
+     delay(TIME);
+    digitalWrite(ledPin_zero, HIGH);
+   delay(TIME);
+    digitalWrite(ledPin_zero, LOW);
+      digitalWrite(ledPin_one, HIGH);
+     delay(TIME);
+      digitalWrite(ledPin_zero, HIGH);
+      digitalWrite(ledPin_one, HIGH);
+     delay(TIME);
+    digitalWrite(ledPin_zero, LOW);
+    digitalWrite(ledPin_one, LOW);
+  digitalWrite(ledPin_two, LOW);
+    digitalWrite(ledPin_three, HIGH);
+     delay(TIME);
+   digitalWrite(ledPin_zero, HIGH);
+   delay(TIME);
+    digitalWrite(ledPin_zero, LOW);
+      digitalWrite(ledPin_one, HIGH);
+     delay(TIME);
+      digitalWrite(ledPin_zero, HIGH);
+      digitalWrite(ledPin_one, HIGH);
+     delay(TIME);
+   digitalWrite(ledPin_zero, LOW);
+      digitalWrite(ledPin_one, LOW);
+        digitalWrite(ledPin_two, HIGH);
+     delay(TIME);
+    digitalWrite(ledPin_zero, HIGH);
+   delay(TIME);
+    digitalWrite(ledPin_zero, LOW);
+      digitalWrite(ledPin_one, HIGH);
+     delay(TIME);
+      digitalWrite(ledPin_zero, HIGH);
+      digitalWrite(ledPin_one, HIGH);
+     delay(TIME);
+   digitalWrite(ledPin_zero, LOW);
+    digitalWrite(ledPin_one, LOW);
+  digitalWrite(ledPin_two, LOW);
+  digitalWrite(ledPin_three, LOW);
+  digitalWrite(ledPin_four, HIGH);
+       delay(TIME);
+   digitalWrite(ledPin_zero, HIGH);
+   delay(TIME);
+    digitalWrite(ledPin_zero, LOW);
+      digitalWrite(ledPin_one, HIGH);
+     delay(TIME);
+      digitalWrite(ledPin_zero, HIGH);
+      digitalWrite(ledPin_one, HIGH);
+     delay(TIME);
+   digitalWrite(ledPin_zero, LOW);
+      digitalWrite(ledPin_one, LOW);
+        digitalWrite(ledPin_two, HIGH);
+     delay(TIME);
+    digitalWrite(ledPin_zero, HIGH);
+   delay(TIME);
+    digitalWrite(ledPin_zero, LOW);
+      digitalWrite(ledPin_one, HIGH);
+     delay(TIME);
+      digitalWrite(ledPin_zero, HIGH);
+      digitalWrite(ledPin_one, HIGH);
+     delay(TIME);
+    digitalWrite(ledPin_zero, LOW);
+    digitalWrite(ledPin_one, LOW);
+  digitalWrite(ledPin_two, LOW);
+    digitalWrite(ledPin_three, HIGH);
+     delay(TIME);
+   digitalWrite(ledPin_zero, HIGH);
+   delay(TIME);
+    digitalWrite(ledPin_zero, LOW);
+      digitalWrite(ledPin_one, HIGH);
+     delay(TIME);
+      digitalWrite(ledPin_zero, HIGH);
+      digitalWrite(ledPin_one, HIGH);
+     delay(TIME);
+   digitalWrite(ledPin_zero, LOW);
+      digitalWrite(ledPin_one, LOW);
+        digitalWrite(ledPin_two, HIGH);
+     delay(TIME);
+    digitalWrite(ledPin_zero, HIGH);
+   delay(TIME);
+    digitalWrite(ledPin_zero, LOW);
+      digitalWrite(ledPin_one, HIGH);
+     delay(TIME);
+      digitalWrite(ledPin_zero, HIGH);
+      digitalWrite(ledPin_one, HIGH);
+     delay(TIME);
+
+
+
+
+
+
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+
+
+}
+
+
+  
